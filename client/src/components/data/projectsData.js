729de6bcb5cb12d3e05e097f6f1e05fbb143a3ef
Ritik Chaurasiya <ritikchaurasiya2005@gmail.com>
2026-06-28 16:35:08 +0530

    add one project

--- client/src/components/data/projectsData.js
+++ client/src/components/data/projectsData.js
@@ -1,12 +1,12 @@
 const projectsData = [
   {
     _id: "1",
-    title: "IIMM Ayodhya Website",
-    description: "Modern responsive college website built with HTML & Bootstrap.",
+    title: "CV Store",
+    description: "CV Store is a MERN Stack application for creating and managing professional resumes. It enables users to build ATS-friendly resumes, choose templates, edit information, and download resumes in PDF format with secure authentication.",
     image: "/images/IIMM.png",
-    techStack: ["HTML", "CSS", "Bootstrap"],
-    github: "https://github.com/ritik-chaurasiya/IIMM-ayodhya",
-    demo: "https://ritik-chaurasiya.github.io/IIMM-ayodhya/"
+    techStack: ["HTML", "CSS", "JavaScript", "Bootstrap","React", "Node.js", "Express", "MongoDB","JWT Authentication", "Cloudinary Image Upload", "Google OAuth Authentication"],
+    github: "https://github.com/ritik-chaurasiya/CV-Strore",
+    demo: "https://cv-strore.vercel.app/"
   },
 
 {
@@ -28,7 +28,16 @@ const projectsData = [
   techStack: ["HTML", "CSS", "Bootstrap", "JavaScript"],
   github: "https://github.com/ritik-chaurasiya/tic-tac_toe",
   demo: "https://ritik-chaurasiya.github.io/tic-tac_toe/"
-}
+  },
+  {
+ _id: "4",
+    title: "IIMM Ayodhya Website",
+    description: "Modern responsive college website built with HTML & Bootstrap.",
+    image: "/images/IIMM.png",
+    techStack: ["HTML", "CSS", "Bootstrap"],
+    github: "https://github.com/ritik-chaurasiya/IIMM-ayodhya",
+    demo: "https://ritik-chaurasiya.github.io/IIMM-ayodhya/"
+  }
 
 
 
